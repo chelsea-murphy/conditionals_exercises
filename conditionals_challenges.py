e9bbc524f4a58c9cd8d9f3501a6db19c7b87cab2
CHELSEAS <chelsea@playbook.coach>
2025-10-04 12:20:39 +1000

    completed challenges

--- conditionals_challenges.py
+++ conditionals_challenges.py
@@ -9,8 +9,7 @@ def can_climb(has_helmet: bool, has_rope: bool):
     Returns:
         - a boolean that is true when the climber is allowed to climb, and false otherwise.
     """
-
-    pass
+    return has_helmet and has_rope
 
 def red_light_camera(light_colour: str, car_detected: bool):
     """A function to determine whether or not a driver should get a ticket 
@@ -24,7 +23,7 @@ def red_light_camera(light_colour: str, car_detected: bool):
 
     Returns: A boolean that is true if a car ran the red light, and false otherwise.
     """
-    pass
+    return light_colour == "Red" and car_detected
 
 def can_ride_rollercoaster(rider_height: float):
     """Only people who are over 120cm in height can ride the rollercoaster.
@@ -35,7 +34,7 @@ def can_ride_rollercoaster(rider_height: float):
     Returns: a boolean representing whether or not the prospective rider is allowed on the rollercoaster.
     """
 
-    pass
+    return rider_height > 120
 
 def login(password: str):
     """Only users who enter the password "quartzgleam?1" can log in successfully.
@@ -47,4 +46,4 @@ def login(password: str):
         - a boolean representing whether or not the user's password was correct.
     """
 
-    pass
+    return password == "quartzgleam?1"
